75175bab677e39aca1f188bc5aecb6395b58a696
Logan Lentz <logan.lentz1@gmail.com>
2023-12-20 06:51:09 -0600

    Add files via upload

---
+++ loadmodel.py
@@ -0,0 +1,35 @@
+from huggingface_hub import snapshot_download
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# Set the default tensor type to float16 for mixed precision (reduces memory usage)
+torch.set_default_dtype(torch.float16)
+
+# Define the prompt
+prompt = "Name the fastest plane on record"
+
+# Set the path to the model directory (update this path according to your Docker setup)
+model_path = "/usr/src/app/model/models--amgadhasan--phi-2/snapshots/673ea632ab7f63050d40a457dba895ce92efb7ae"
+
+# Load the tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True)
+
+# Move model to GPU if CUDA is available
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+
+# Text generation function
+def generate(prompt: str, generation_params: dict = {"max_length": 200}) -> str:
+    try:
+        torch.cuda.empty_cache()  # Clear CUDA cache
+        inputs = tokenizer(prompt, return_tensors="pt").to(device)
+        outputs = model.generate(**inputs, **generation_params)
+        completion = tokenizer.batch_decode(outputs)[0]
+        return completion
+    except Exception as e:
+        return f"Error generating text: {str(e)}"
+
+# Generate and print the result
+result = generate(prompt)
+print(result)
